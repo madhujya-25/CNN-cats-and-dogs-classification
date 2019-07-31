4bb1eefe8032fc5973d3c3e0d137c26a71c6e4dd
maximus0069 <48417119+maximus0069@users.noreply.github.com>
2019-08-01 01:29:50 +0530

    adding first version

---
+++ mine.py
@@ -0,0 +1,54 @@
+
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+classifier = Sequential()
+
+classifier.add(Convolution2D(32, 3, 3, input_shape = (32, 32, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2,2)))
+classifier.add(Flatten())
+classifier.add(Dense(output_dim =64, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+from keras.preprocessing.image import ImageDataGenerator
+ 
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_set = train_datagen.flow_from_directory(
+        'dataset/training_set',
+        target_size=(32,32),
+        batch_size=64,
+        class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+        'dataset/test_set',
+        target_size=(32,32),
+        batch_size=64,
+        class_mode='binary')
+
+classifier.fit_generator(
+        train_set,
+        steps_per_epoch=8000,
+        epochs=25,
+        validation_data=test_set,
+        nb_val_samples=2000)
+
+
+
+
+
+
+
+
+
